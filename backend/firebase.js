7a4ad903fa55dbf9e1bfc8f109353371376dfd4a
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2024-12-01 15:52:11 +0530

    multiple edits

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/honors-minors-application-7681836bed5c.json'); // Download from Firebase Console
+const serviceAccount = require('./config/honors-minors-application-firebase-adminsdk-yi8in-d6e530c14a.json'); // Download from Firebase Console
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
